a631081a2f4b765bfb01ee106553e6dfd243f29b
Aoko <hejunqin233@gmail.com>
2019-06-15 14:07:13 +0800

    (refactor): withReducer 类型声明调整

--- src/utils/decorators/index.ts
+++ src/utils/decorators/index.ts
@@ -1,9 +1,10 @@
+import { ComponentType } from "react";
 import { Reducer } from "redux";
 import { injectReducer } from "../../config/store";
 
 // 将page对应的reducer注入到store中
-export const withReducer = (key: string, reducer: Reducer) => (
-  WrappedComponent: any
+export const withReducer = (key: string, reducer: Reducer) => <P extends {}>(
+  WrappedComponent: ComponentType<P>
 ) => {
   injectReducer(key, reducer);
 };
