30003990a4858a53a248c84cbcbaa8455154d848
Aoko <hejunqin233@gmail.com>
2019-06-18 21:46:21 +0800

    (refactor): reducer注入功能调整

--- src/pages/transfer/operation/index.tsx
+++ src/pages/transfer/operation/index.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-import { withReducer } from "../../../utils/decorators";
+import React, { useEffect } from "react";
+import { injectReducer } from "../../../config/store";
 import Header from "./components/Header";
 import Table from "./components/Table";
 import reducer from "./store/reducer";
 
-@withReducer("transferOperation", reducer)
-class TransferOperation extends React.Component {
-  public render() {
-    return (
-      <div>
-        <Header />
-        <Table />
-      </div>
-    );
-  }
-}
+const TransferOperation: React.FC = () => {
+  useEffect(() => injectReducer("transferOperation", reducer), []);
+  return (
+    <div>
+      <Header />
+      <Table />
+    </div>
+  );
+};
 
 export default TransferOperation;
