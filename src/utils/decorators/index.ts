7a89af9b1a10bf15168c1b038d402fdde863c6b6
Aoko <hejunqin233@gmail.com>
2019-06-18 21:43:35 +0800

    withRed

--- src/utils/decorators/index.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentType } from "react";
-import { injectReducer } from "../../config/store";
-
-// 将page对应的reducer注入到store中
-export const withReducer = (key: string, reducer: any) => <P extends {}>(
-  WrappedComponent: ComponentType<P>
-) => {
-  injectReducer(key, reducer);
-};
